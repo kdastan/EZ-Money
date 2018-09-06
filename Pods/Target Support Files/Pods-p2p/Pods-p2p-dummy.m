26a1c10854e5e19714d9721407bb9718137c8b5e
kdastan <kumardastan@gmail.com>
2018-09-06 14:58:25 +0600

    feat: add and setup gitignore file

--- Pods/Target Support Files/Pods-p2p/Pods-p2p-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_p2p : NSObject
-@end
-@implementation PodsDummy_Pods_p2p
-@end
--- Pods/Target Support Files/SwiftValidator/SwiftValidator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftValidator : NSObject
-@end
-@implementation PodsDummy_SwiftValidator
-@end
